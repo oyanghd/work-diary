ffe50d9a4c477ec63d7b88b8209fd8d6a347b875
Oyanghd <885299886@qq.com>
2024-01-02 23:10:46 +0800

    feat(append_log.sh): feat background process

--- append_log.sh
+++ append_log.sh
@@ -3,19 +3,18 @@
 host_name=$(hostname)
 current_time=$(date "+%Y-%m-%d %H:%M:%S")
 
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 log_file="$script_dir/$host_name.log"
 
 if [ -z "$SHELL_LOG_APPENDED" ]; then
-    echo "hostname: $host_name" >> "$log_file"
-    echo "systemtime: $current_time" >> "$log_file"
+	echo "hostname: $host_name" >>"$log_file"
+	echo "systemtime: $current_time" >>"$log_file"
 
-    export SHELL_LOG_APPENDED="true"
+	export SHELL_LOG_APPENDED="true"
 
-    cd "$script_dir"
+	cd "$script_dir"
 
-    git add .
-    git commit -m "hi($host_name.log): login in $host_name at $current_time"
-    git push
+	git add .
+	git commit -m "hi($host_name.log): login in $host_name at $current_time" &
+	git push
 fi
-
